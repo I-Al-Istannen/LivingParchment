1c3e403655b8e000e91a504378b2742bff1170fc
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2018-05-20 22:58:40 +0200

    Set base theme

---
+++ app/src/main/java/me/ialistannen/livingparchment/feature/add/BookAddActivity.kt
@@ -0,0 +1,13 @@
+package me.ialistannen.livingparchment.feature.add
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import me.ialistannen.livingparchment.R
+
+class BookAddActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_book_add)
+    }
+}
--- app/src/main/java/me/ialistannen/livingparchment/feature/mainscreen/MainActivity.kt
+++ app/src/main/java/me/ialistannen/livingparchment/feature/mainscreen/MainActivity.kt
@@ -1,6 +1,9 @@
 package me.ialistannen.livingparchment.feature.mainscreen
 
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import kotlinx.android.synthetic.main.activity_main.*
 import me.ialistannen.livingparchment.R
 import me.ialistannen.livingparchment.feature.BaseActivity
 import me.ialistannen.livingparchment.feature.BasePresenter
@@ -12,5 +15,12 @@ class MainActivity : BaseActivity(), MainScreenContract.View {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(actionbar as Toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.shared_action_bar, menu)
+        return true
     }
 }
