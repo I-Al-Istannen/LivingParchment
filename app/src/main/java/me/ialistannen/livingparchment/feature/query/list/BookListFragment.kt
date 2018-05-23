e731bf43d4f7abf12cd4d04940c67fd6637d6002
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2018-05-23 14:04:15 +0200

    Add a search bar to the BookListFragment

--- app/src/main/java/me/ialistannen/livingparchment/feature/query/list/BookListFragment.kt
+++ app/src/main/java/me/ialistannen/livingparchment/feature/query/list/BookListFragment.kt
@@ -1,15 +1,15 @@
 package me.ialistannen.livingparchment.feature.query.list
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.SearchView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.fragment_book_query_list.*
 import me.ialistannen.livingparchment.R
 import me.ialistannen.livingparchment.common.model.Book
 import me.ialistannen.livingparchment.feature.BaseFragment
 import me.ialistannen.livingparchment.feature.query.QueryNavigator
+import java.text.DateFormat
 import javax.inject.Inject
 
 class BookListFragment : BaseFragment(), BookListFragmentContract.View {
@@ -25,11 +25,37 @@ class BookListFragment : BaseFragment(), BookListFragmentContract.View {
         return inflater.inflate(R.layout.fragment_book_query_list, container, false)
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         if (books.isNotEmpty()) {
             book_list.books = books
             book_list.setClickListener { presenter.bookSelected(it) }
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+
+        inflater.inflate(R.menu.fragment_book_list_action_bar, menu)
+
+        val view = menu.findItem(R.id.app_bar_search).actionView as SearchView
+        view.queryHint = getString(android.R.string.search_go)
+
+        view.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                book_list.filter(getBookFilter(query))
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                book_list.filter(getBookFilter(newText))
+                return true
+            }
+        })
     }
 
     /**
@@ -60,4 +86,52 @@ class BookListFragment : BaseFragment(), BookListFragmentContract.View {
 
         navigator.displayDetailPage(book)
     }
+
+    private fun getBookFilter(containedText: String): (Book) -> Boolean {
+        if (containedText.isBlank()) {
+            return { true }
+        }
+        return { book ->
+            when {
+                book.title.isContained(containedText) -> true
+                book.genre.any { it.isContained(containedText) } -> true
+                book.authors.any { it.isContained(containedText) } -> true
+                book.isbn.isContained(containedText) -> true
+                book.language.isContained(containedText) -> true
+                book.pageCount.toString().isContained(containedText) -> true
+                book.publisher.isContained(containedText) -> true
+                formatPublishedDate(book).isContained(containedText) -> true
+                book.extra.containsString(containedText) -> true
+                else -> false
+            }
+        }
+    }
+
+    private fun formatPublishedDate(book: Book) =
+            DateFormat.getDateInstance(DateFormat.LONG).format(book.published)
+
+    private fun Map<String, Any>.containsString(string: String): Boolean {
+        for ((_, value) in entries) {
+            if (value is Iterable<*>) {
+                if (value.any { it.toString().isContained(string) }) {
+                    return true
+                }
+            } else if (value.toString().isContained(string)) {
+                return true
+            }
+        }
+
+        return false
+    }
+
+    private fun String.isContained(other: String): Boolean {
+        return this
+                .replace(CLEANUP_REGEX, "")
+                .contains(
+                        other.replace(CLEANUP_REGEX, ""),
+                        true
+                )
+    }
 }
+
+private val CLEANUP_REGEX = "[.,\\-\\s?!()]".toRegex()
