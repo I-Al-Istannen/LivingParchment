b88d7a859c285bdcb5599be5036a6543c3f15de7
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2018-05-27 18:48:55 +0200

    Removed the editable mode

--- app/src/main/java/me/ialistannen/livingparchment/feature/edit/EditScreenPresenter.kt
+++
@@ -1,8 +0,0 @@
-package me.ialistannen.livingparchment.feature.edit
-
-import me.ialistannen.livingparchment.feature.BasePresenter
-import javax.inject.Inject
-
-class EditScreenPresenter @Inject constructor(
-        private val view: EditScreenContract.View
-) : BasePresenter(), EditScreenContract.Presenter
