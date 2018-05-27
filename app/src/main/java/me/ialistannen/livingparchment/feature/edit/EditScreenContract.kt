44ef02bb876d1614f5b4d77bf339303425fc6a0f
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2018-05-27 18:47:42 +0200

    Futile attempts to introduce an edit mode

---
+++ app/src/main/java/me/ialistannen/livingparchment/feature/edit/EditScreenContract.kt
@@ -0,0 +1,10 @@
+package me.ialistannen.livingparchment.feature.edit
+
+import me.ialistannen.livingparchment.feature.BaseView
+
+interface EditScreenContract {
+
+    interface View : BaseView
+
+    interface Presenter : me.ialistannen.livingparchment.feature.Presenter
+}
